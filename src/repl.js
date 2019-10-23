94a1bd86526a39b12aa8a64ef4a6202cde33af6b
UddeshJain <jainuddesh35@gmail.com>
2019-10-23 12:59:46 +0530

    Basic operations are working

--- src/repl.js
+++ src/repl.js
@@ -3,12 +3,30 @@ const chalk = require('chalk');
 
 const { parseAndEvaluate } = require('./parse-and-evaluate');
 
-const repl = async () => {};
+const askQuestions = () => {
+  const questions = [
+    { name: "COMMAND", type: "input", message: chalk.blue(">") }
+  ];
+  return prompt(questions);
+}
+
+const repl = async () => {
+  try {
+    const answers = await askQuestions();
+    const { COMMAND } = answers;
+    if (COMMAND.trim()) {
+      console.log(chalk.yellow(parseAndEvaluate(COMMAND)))
+    }
+  } catch (error) {
+    console.error(error);
+  }
+  repl();
+};
 
 if (require.main === module) {
   console.log(
     chalk.red(
-      `Welcome to the ${chalk.bgYellow('Dropbear')} Programming Language`,
+      `Welcome to the ${chalk.bgYellow('(Still Searching for name)')} Programming Language`,
     ),
   );
   repl();
--- src/tokenize.js
+++ src/tokenize.js
@@ -6,6 +6,67 @@ const {
   isQuote,
 } = require('./identify');
 
-const tokenize = () => {};
+const tokenize = (input) => {
+  const token = [];
+  let cursor = 0;
+  while (cursor < input.length) {
+    const character = input[cursor]
+
+    if (isParenthesis(character)) {
+      token.push({
+        type: "Parenthesis",
+        value: character
+      });
+      cursor++
+      continue;
+    };
+
+    if (isWhitespace(character)) {
+      cursor++
+      continue;
+    };
+
+    if (isNumber(character)) {
+      let number = character
+      while (isNumber(input[++cursor])) {
+        number += input[cursor]
+      }
+      token.push({
+        type: "Number",
+        value: parseInt(number, 10)
+      });
+      continue
+    };
+
+    if (isLetter(character)) {
+      let symbol = character;
+      while (isLetter(input[++cursor])) {
+        symbol += input[cursor]
+      }
+      token.push({
+        type: "Name",
+        value: symbol
+      });
+      continue;
+    };
+
+    if (isQuote(character)) {
+      let string = '';
+      while (!isQuote(input[++cursor])) {
+        string += input[cursor]
+      }
+      token.push({
+        type: "String",
+        value: string
+      })
+      cursor++
+      continue
+    };
+
+    throw new Error(`${character} is not vailid.`)
+  }
+
+  return token;
+};
 
 module.exports = { tokenize };
