e2ab7569c774da65ff184702349fdf78b5074d62
Uddesh Jain <jainuddesh35@gmail.com>
2020-04-24 11:42:04 +0530

    Added variable declaration feature.

--- src/to-javascript.js
+++ src/to-javascript.js
@@ -1,9 +1,30 @@
 const generate = require('@babel/generator').default;
 const { traverse } = require('./traverse');
 
-const babelVisitor = {};
+const babelVisitor = {
+  CallExpression: {
+    enter({ node }) {
+      node.callee = { type: 'Identifier', name: node.name };
+    },
+  },
+  VariableDeclaration: {
+    enter({ node }) {
+      node.kind = 'let';
+      node.declarations = [
+        {
+          type: 'VariableDeclarator',
+          id: node.identifier,
+          init: node.assignment,
+        },
+      ];
+    },
+  },
+};
 
-const toJavaScript = ast => {};
+const toJavaScript = (ast) => {
+  traverse(ast, babelVisitor);
+  return generate(ast).code;
+};
 
 module.exports = {
   toJavaScript,
